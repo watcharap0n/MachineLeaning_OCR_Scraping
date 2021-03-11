a4ca9ccf944cda0c711fa1cf7e65894231ffde41
watcharapon weeraborirak <s5919410022@sau.ac.th>
2021-03-11 11:12:49 +0700

    Update spam_prediction.py

--- spam_prediction.py
+++ spam_prediction.py
@@ -4,6 +4,8 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder, scale, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 data = pd.read_csv('datasets/data.csv')
 
@@ -28,3 +30,16 @@ print(prediction)
 # acc = accuracy_score(y_test, prediction)
 target_name = ['not spam', 'spam']
 print(target_name[prediction[0]])
+
+X_train_pd = pd.DataFrame(x_train, columns=['x', 'y'])
+y_train_pd = pd.DataFrame(y_train, columns=['class'])
+
+df = pd.concat([X_train_pd, y_train_pd], axis=1)
+df["class"] = df["class"].astype(str)
+df.plot.scatter(x="x", y="y")
+plt.show()
+# plt.plot(df['y'], df['y'], 'ro')
+# plt.axis([df['class']])
+# plt.show()
+# fig = px.scatter(df, x="x", y="y", color="class")
+# fig.show()
